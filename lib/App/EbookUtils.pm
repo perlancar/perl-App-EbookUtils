8671e3093f3e68a9cf4308bb40f54ec14b23dd2a
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-02-10 14:21:56 +0700

    Initial commit

---
+++ lib/App/EbookUtils.pm
@@ -0,0 +1,25 @@
+package App::EbookUtils;
+
+use 5.010001;
+use strict;
+use warnings;
+#use Log::ger;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Command-line utilities related to ebooks
+
+=head1 SYNOPSIS
+
+Currently empty.
+
+
+=head1 SEE ALSO
+
+L<App::PDFUtils> distribution includes some utilities like L<epub2pdf>.
+
+=cut
